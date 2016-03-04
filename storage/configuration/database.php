be5d9815e619794373c2880c95dcb7b50a4a166b
scott-simpson <hello@scottsimpson.co>
2016-03-04 18:02:26 -0500

    First commit

---
+++ storage/configuration/database.php
@@ -0,0 +1,9 @@
+<?php
+	return array(
+		'hostname' => 'localhost',
+		'database' => 'koken',
+		'username' => 'koken',
+		'password' => 'oox6To7iepah',
+		'prefix' => 'koken_',
+		'socket' => ''
+	);
---
+++ storage/configuration/key.php
@@ -0,0 +1,6 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+// Do not edit or remove this file unless advised by Koken support
+// support@koken.me
+
+return '7f55b53d3666631ad165511efd5a7d24';
